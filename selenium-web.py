5c3c68900fb529a73b020d47f9bcfe5d1369d48f
thakureie <36667325+thakureie@users.noreply.github.com>
2021-06-02 21:21:10 +0530

    Update selenium-web.py

--- selenium-web.py
+++ selenium-web.py
@@ -3,15 +3,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-url = "https://pdit-tools.oracleoutsourcing.com/apex/f?p=110:3:15004997345439::NO:RP,3::"
+url = "f?p=110:3:15004997345439::NO:RP,3::"
 driver = webdriver.Firefox()
 #time.sleep(30)
 driver.get(url)
 time.sleep(30)
 u = driver.find_element_by_tag_name('Username')
-u.send_keys('abhimanyu.thakur@oracle.com')
+u.send_keys('abhimanyu.thakur')
 p = driver.find_element_by_name('Password')
-p.send_keys('Pawan1@jha')
+p.send_keys('')
 p.send_keys(Keys.RETURN)
 '''
 '''
@@ -28,12 +28,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-url = "https://pdit-tools.oracleoutsourcing.com/apex/f?p=110:3:15004997345439::NO:RP,3::"
+url = "
 driver = webdriver.Firefox()
 driver.get(url)
 try:
     element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "Username")))
-    EC.send_keys('abhimanyu.thakur@oracle.com')
+    EC.send_keys('')
 finally:
     driver.quit()
